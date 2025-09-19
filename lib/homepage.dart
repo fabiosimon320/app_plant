22ef75a15f4efc389996ddee2a8b02a8e23a4d9c
fabiosimon320 <firesaimon48@gmail.com>
2025-09-19 14:34:43 +0200

    Add home page

---
+++ lib/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/widgets/mybottomnavigationbar.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: MyBottomNavigationBar(),),
+
+    );
+  }
+
+}
