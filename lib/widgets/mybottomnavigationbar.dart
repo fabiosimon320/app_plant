f8b57d73491ad7938a6afa64a76d8cc4b59ec1db
fabiosimon320 <firesaimon48@gmail.com>
2025-09-19 14:38:15 +0200

    Minor changes in main.dart and navigation bar widget

--- lib/widgets/mybottomnavigationbar.dart
+++ lib/widgets/mybottomnavigationbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+import 'package:plant_app/screens/plantscreen.dart';
 
 class MyBottomNavigationBar extends StatelessWidget {
   const MyBottomNavigationBar({super.key});
@@ -9,7 +10,7 @@ class MyBottomNavigationBar extends StatelessWidget {
     return PersistentTabView(
         tabs: [
           PersistentTabConfig(
-            screen: Center(),
+            screen: PlantScreen(),
             item: ItemConfig(
               icon: Icon(Icons.home),
               title: "Le tue piante",
@@ -48,24 +49,6 @@ class MyBottomNavigationBar extends StatelessWidget {
     );
 
   }
-
-
-
- /* Widget build(BuildContext context) {
-    return BottomNavigationBar(
-
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.add_a_photo), label: 'Inserisci pianta'),
-        BottomNavigationBarItem(icon: Icon(Icons.eco), label: 'Piante consigliate'),
-      ],
-
-      selectedIconTheme: IconThemeData(
-          color: Colors.green
-      ),
-    );
-  }*/
-
 
 
 
