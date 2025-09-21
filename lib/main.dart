11e267baf50c4d53091b1ab1c9edf3c6b75ead4a
fabiosimon320 <firesaimon48@gmail.com>
2025-09-21 12:04:06 +0200

    Update plant model to include ID field

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:plant_app/homepage.dart';
+import 'package:plant_app/services/geminiservice.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Gemini().initGemini();
+  runApp(
+      ProviderScope(
+          child: const MyApp()
+      ),
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:uuid/uuid.dart';
 
 class Plant {
   final String name;
+  final String scientificname;
   final ImageProvider image;
   final String description;
+  final String id;
 
-  const Plant({
+   Plant({
+    String? id,
     required this.name,
+    required this.scientificname,
     required this.image,
     required this.description,
-  });
+  }) : id = id ?? Uuid().v4();
 }
