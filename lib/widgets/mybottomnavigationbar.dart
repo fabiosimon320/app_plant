bf2bab936aec126474dde302cccf99b83fa0f3e0
fabiosimon320 <firesaimon48@gmail.com>
2025-09-21 12:01:05 +0200

    Add controller to BottomNavigationBar and AddPlant screen

--- lib/widgets/mybottomnavigationbar.dart
+++ lib/widgets/mybottomnavigationbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+import 'package:plant_app/screens/addplantscreen.dart';
 import 'package:plant_app/screens/plantscreen.dart';
 
 class MyBottomNavigationBar extends StatelessWidget {
@@ -7,6 +8,8 @@ class MyBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final PersistentTabController controller = PersistentTabController(initialIndex: 0);
     return PersistentTabView(
         tabs: [
           PersistentTabConfig(
@@ -18,7 +21,7 @@ class MyBottomNavigationBar extends StatelessWidget {
             ),
           ),
           PersistentTabConfig(
-            screen: Center(),
+            screen: AddPlantScreen(controller: controller),
             item: ItemConfig(
               icon: Icon(Icons.add_a_photo),
               title: "Aggiungi pianta",
@@ -36,15 +39,6 @@ class MyBottomNavigationBar extends StatelessWidget {
         ],
         navBarBuilder: (navBarConfig) => Style1BottomNavBar(
           navBarConfig: navBarConfig,
-          navBarDecoration: NavBarDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.shade400,
-                blurRadius: 0.5,
-                offset: Offset(0,-1),
-              )
-            ]
-          )
         )
     );
 
