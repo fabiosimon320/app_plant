e1a75112e8073ed1ec18fcb7e16e0c06513a5651
fabiosimon320 <firesaimon48@gmail.com>
2025-09-25 20:31:41 +0200

    Add notifier service init on main

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:plant_app/homepage.dart';
 import 'package:plant_app/services/geminiservice.dart';
+import 'package:plant_app/services/notification_service.dart';
+
 
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService().init();
   await Gemini().initGemini();
   runApp(
       ProviderScope(
